02931f9273d34b65618b6e1bc6bf96b9b87adbc3
allen <dev@awu.me>
2024-11-23 11:47:56 +1300

    capture.sh updates; and for 3 cameras

--- capture.sh
+++ capture.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+devices=("/dev/video0" "/dev/video2" "/dev/video4")
+fragments=("A" "B" "C")
+
 timestamp=$(date +%s)
 start_time=$(date +%s%3N)
 start_time_pretty=$(date +"%T.%N")
-devices=("/dev/video0" "/dev/video2" "/dev/video4" "/dev/video6")
-fragments=("A" "B" "C" "D")
 
 # Capture images in parallel
 for i in "${!devices[@]}"; do
-    ffmpeg -loglevel error -f video4linux2 -i "${devices[$i]}" -frames:v 1 -s 435x309 "images/${timestamp}_${fragments[$i]}.jpg" &
+    ffmpeg -loglevel error -f video4linux2 -i "${devices[$i]}" -frames:v 1 -s 1920x1080 "images/${timestamp}_${fragments[$i]}.jpg" &
     sleep 0.1
 done
-
-# Wait for all background processes to finish
 wait
 
 # Calculate elapsed time
@@ -29,10 +28,7 @@ printf "Start time:                %s\n" "$start_time_pretty"
 ls -lh --time-style=full-iso images/"${timestamp}"_* | awk '{print "--", $9, $7}'
 printf "Elapsed time:              %d ms\n" "$elapsed"
 
-# Success/Error Message
-if [ "$captured_count" -eq "$expected_count" ]; then
-    printf "Success!\n"
-else
+if [ "$captured_count" -ne "$expected_count" ]; then
     printf "Error: Captured %d instead of %d\n" "$captured_count" "$expected_count"
 fi
 echo ""
